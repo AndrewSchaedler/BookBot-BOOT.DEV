b93574c5796362f1e8d1fdd5a9dfae6cd6e3634b
AndrewSchaedler <schaedlerandrew@gmail.com>
2025-07-08 15:55:49 -0300

    Add get_book_text and count_words to stats.py, update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def get_book_text(filepath):
-    with open(filepath, "r", encoding="utf-8") as f:
-        return f.read()
-
-def count_words(text):
-    words = text.split()
-    return len(words)
+from stats import get_book_text, count_words
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
